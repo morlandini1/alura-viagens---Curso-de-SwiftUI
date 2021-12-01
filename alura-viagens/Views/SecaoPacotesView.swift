2dd23c49f1b18437e215eab2ce9dca1984d7858e
MARCIO ORLANDINI <morlandini1@gmail.com>
2021-12-01 09:45:21 -0300

    divisão de seções

---
+++ alura-viagens/Views/SecaoPacotesView.swift
@@ -0,0 +1,21 @@
+//
+//  SecaoPacotesView.swift
+//  alura-viagens
+//
+//  Created by Marcio Orlandini on 01/12/21.
+//  Copyright © 2021 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecaoPacotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecaoPacotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecaoPacotesView()
+    }
+}
