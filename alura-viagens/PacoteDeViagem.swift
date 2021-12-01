abd45dee039808e464126a8901437a65c0f8331c
MARCIO ORLANDINI <morlandini1@gmail.com>
2021-12-01 08:59:03 -0300

    atualiza

---
+++ alura-viagens/PacoteDeViagem.swift
@@ -0,0 +1,21 @@
+//
+//  PacoteDeViagem.swift
+//  alura-viagens
+//
+//  Created by Marcio Orlandini on 30/11/21.
+//  Copyright © 2021 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct PacoteDeViagem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PacoteDeViagem_Previews: PreviewProvider {
+    static var previews: some View {
+        PacoteDeViagem()
+    }
+}
---
+++ alura-viagens/Views/PacotesViagensView.swift
@@ -0,0 +1,21 @@
+//
+//  PacotesViagensView.swift
+//  alura-viagens
+//
+//  Created by Marcio Orlandini on 30/11/21.
+//  Copyright © 2021 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct PacotesViagensView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PacotesViagensView_Previews: PreviewProvider {
+    static var previews: some View {
+        PacotesViagensView()
+    }
+}
