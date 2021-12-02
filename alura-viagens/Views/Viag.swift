eae8940e8f1e32c4d3cf559ecadecf21dd8f74d0
MARCIO ORLANDINI <morlandini1@gmail.com>
2021-12-02 08:09:50 -0300

    create

---
+++ alura-viagens/Views/Viag.swift
@@ -0,0 +1,21 @@
+//
+//  Viag.swift
+//  alura-viagens
+//
+//  Created by Marcio Orlandini on 02/12/21.
+//  Copyright © 2021 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct Viag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Viag_Previews: PreviewProvider {
+    static var previews: some View {
+        Viag()
+    }
+}
