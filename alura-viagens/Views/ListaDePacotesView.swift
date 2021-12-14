d6ed68902d9c4c93573a2c34301e7f16a3f306d8
MARCIO ORLANDINI <morlandini1@gmail.com>
2021-12-14 13:33:07 -0300

    View ListaDePacotesVews

---
+++ alura-viagens/Views/ListaDePacotesView.swift
@@ -0,0 +1,21 @@
+//
+//  ListaDePacotesView.swift
+//  alura-viagens
+//
+//  Created by Marcio Orlandini on 14/12/21.
+//  Copyright © 2021 Ândriu Felipe Coelho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListaDePacotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListaDePacotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListaDePacotesView()
+    }
+}
